3c01354bcbb617b6efd6917cadac82bb2b1cccb8
reyqn <reyqn@protonmail.com>
2016-08-03 12:32:20 +0200

    fix beehive

--- runserver.py
+++ runserver.py
@@ -55,10 +55,10 @@ if __name__ == '__main__':
         logging.getLogger('pgoapi').setLevel(logging.DEBUG)
         logging.getLogger('rpc_api').setLevel(logging.DEBUG)
 
-    if  args.latitude is not None and args.longitude is not None:                                                                                                    
+    if  (args.latitude is not None and args.longitude is not None):                                                                                                    
         position = [float(args.latitude), float(args.longitude), 0]
     else:
-        position = get_pos_by_name(args.location)
+        position = util.get_pos_by_name(args.location)
         if not any(position):
             log.error('Could not get a position by name, aborting')
             sys.exit()
